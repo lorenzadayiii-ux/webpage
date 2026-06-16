3a2a7ed45a19ada0bb116645283a8b3d44178ab8
Lorenza Day <Lorenzadayiii@gmail.com>
2026-06-16 14:16:43 -0500

    updated my portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const helloButton = document.getElementById("helloButton");
+const message = document.getElementById("message");
+
+helloButton.addEventListener("click", function () {
+  message.textContent = "Hello! Welcome to my portfolio page.";
+});
+
+const themeButton = document.getElementById("themeButton");
+
+themeButton.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+
+  if (document.body.classList.contains("dark")) {
+    themeButton.textContent = "Light Mode";
+  } else {
+    themeButton.textContent = "Dark Mode";
+  }
+});
